8b0f5856cedc71eb4b4cf0d1daf0bfed7ab2e741
Eugene Rabinovitch <eugene.rab@gmail.com>
2019-12-12 14:08:05 +0100

    Humble beginnings

---
+++ index.js
@@ -0,0 +1,18 @@
+export const COLOR_BLUE = "#1e70a1";
+export const COLOR_DARK_BLUE = "#0e3247";
+export const COLOR_LIGHT_BLUE = "#e6edf2";
+
+export const COLOR_ORANGE = "#ff9e1b";
+export const COLOR_RED = "#f24011";
+export const COLOR_GREEN = "#14872c";
+export const COLOR_WHITE = "#ffffff";
+
+export const COLOR_GREY20 = "#333333";
+export const COLOR_GREY40 = "#666666";
+export const COLOR_GREY60 = "#999999";
+export const COLOR_GREY90 = "#e6e6e6";
+export const COLOR_GREY96 = "#f5f6f7";
+
+export const COLOR_HOVER_BLUE = "#1a5e86";
+export const COLOR_HOVER_LIGHT_BLUE = "#e1e8ed";
+export const COLOR_HOVER_GREY85 = "#d9d9d9";
